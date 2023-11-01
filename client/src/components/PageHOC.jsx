71831f45f08166e8ea0c86c03c839d110a8fbeed
saikiran0701 <reddysai616@gmail.com>
2023-11-01 20:01:45 +0530

    Higher-order-component

---
+++ client/src/components/PageHOC.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import {logo , heroImg} from '../assets'
+import styles from '../styles'
+
+
+const PageHOC = (Component , title , description) => () => {
+    const navigate = useNavigate();
+  return (
+    <div className={styles.hocContainer}>
+        <div className={styles.hocContentBox}>
+            <img src={logo} alt="logo" className={styles.hocLogo} onClick={() => {navigate('/')}} />
+            <div className={styles.hocBodyWrapper}>
+                <div className='flex flex-row w-full'>
+                    <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
+
+                </div>
+                <p className={`${styles.normalText} my-10`}>{description}</p>
+        <Component/>
+        </div>
+        <p className={styles.footerText}>Made by me</p>
+        </div>
+       <div>
+        <img src={heroImg} alt="heroimage" className='w-full object-cover xl:h-full' />
+       </div>
+    </div>
+  )
+}
+
+export default PageHOC
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './page/Home';
+// import Home from './page/Home';
+import { CreateBattle , Home } from './page';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/create-battle" element={<CreateBattle/>}/>
     </Routes>
   </BrowserRouter>,
 );
