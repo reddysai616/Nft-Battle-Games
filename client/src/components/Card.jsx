8f79faff616e6aca3fa551c82cbbaf2f4b686a16
saikiran0701 <reddysai616@gmail.com>
2023-11-06 17:30:11 +0530

    deff-att cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -8,6 +8,12 @@ const Card = ({card , title , restStyles , cardRef, playerTwo}) => {
   return (
     <div className={`${styles.cardContainer} ${restStyles}`}>
         <img src={playerTwo? img2 : img1} alt="card" className={styles.cardImg} />
+        <div className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}>
+            <p className={`${styles.cardPoint} text-yellow-400`}> {card.att}</p>
+        </div>
+        <div className={`${styles.cardPointContainer} sm:left-[21.2%] right-[22%] ${styles.flexCenter}`}>
+            <p className={`${styles.cardPoint} text-yellow-400`}> {card.att}</p>
+        </div>
         
 
     </div>
