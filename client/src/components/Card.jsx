e101267e97f0a93b25df8dea73d823dec948b512
saikiran0701 <reddysai616@gmail.com>
2023-11-11 19:40:53 +0530

    cards-att,deff

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
 import { allCards } from '../assets'
 import styles from '../styles';
 const generateRandomCard = () => allCards[Math.floor(Math.random() *(allCards.length - 1))];
@@ -6,17 +7,23 @@ const img1= generateRandomCard();
 const img2= generateRandomCard();
 const Card = ({card , title , restStyles , cardRef, playerTwo}) => {
   return (
-    <div className={`${styles.cardContainer} ${restStyles}`}>
+    <Tilt>
+
+    <div  ref={cardRef} className={`${styles.cardContainer} ${restStyles}`}>
         <img src={playerTwo? img2 : img1} alt="card" className={styles.cardImg} />
         <div className={`${styles.cardPointContainer} sm:left-[21.2%] left-[22%] ${styles.flexCenter}`}>
-            <p className={`${styles.cardPoint} text-yellow-400`}> {card.att}</p>
+            <p className={`${styles.cardPoint} text-yellow-400`}> {card.def}</p>
         </div>
-        <div className={`${styles.cardPointContainer} sm:left-[21.2%] right-[22%] ${styles.flexCenter}`}>
-            <p className={`${styles.cardPoint} text-yellow-400`}> {card.att}</p>
+        <div className={`${styles.cardPointContainer} sm:right-[15.2%] right-[15%] ${styles.flexCenter}`}>
+            <p className={`${styles.cardPoint} text-red-400`}> {card.att}</p>
         </div>
-        
+        <div className={`${styles.cardTextContainer} ${styles.flexCenter}`}>
+          <p className={styles.cardText}>{title}</p>
+        </div>
 
     </div>
+    </Tilt>
+
   )
 }
 
