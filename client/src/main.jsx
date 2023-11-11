c8b08b31152f0d41cca77eb786bfb636c7abe9ee
saikiran0701 <reddysai616@gmail.com>
2023-11-11 19:40:42 +0530

    router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // import Home from './page/Home';
-import { CreateBattle , Home, JoinBattles , Battle} from './page';
+import { CreateBattle , Home, JoinBattles , Battle, BattleGround} from './page';
 import './index.css';
 import { GlobalContextProvider } from './context';
-
+import { OnboardModal } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <GlobalContextProvider>
+    <OnboardModal/>
   <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/create-battle" element={<CreateBattle/>}/>
       <Route path="/join-battle" element={<JoinBattles/>}/>
+      <Route path="/BattleGround" element={<BattleGround/>}/>
+
       <Route path="/battle/:battleName" element={<Battle/>}/>
 
 
